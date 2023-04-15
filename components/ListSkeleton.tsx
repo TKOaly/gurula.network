4a41f3ab878fc1dbedffd0c4ac4bc4fa11072c4b
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-04-15 14:34:19 +0300

    Make ListSkeleton spacing and sizing match normal text better

--- components/ListSkeleton.tsx
+++ components/ListSkeleton.tsx
@@ -1,6 +1,13 @@
 const skeletonLengths = new Array(10)
   .fill(null)
-  .map(() => Math.random() * 10 + 5);
+  .map(() => Math.random() * 25 + 5);
+
+const LineSkeleton = ({ width }: { width: number }) => (
+  <span style={{ width: `${width}ch` }} className="inline-block flex items-center">
+    <span className="w-0 inline-block text-transparent">A</span>
+    <span className="inline-block grow rounded-sm h-[1em] skeleton-box"></span>
+  </span>
+);
 
 export const ListSkeleton = () => (
   <>{
@@ -8,8 +15,8 @@ export const ListSkeleton = () => (
       .fill(null)
       .map((_, i) => (
         <li key={i} className="py-2 pl-3 pr-1 rounded-md bg-zinc-100 bg-opacity-5 flex gap-2 mb-2 items-center">
-          <span className="text-transparent rounded-sm skeleton-box">86x</span>
-          <span style={{ width: `${skeletonLengths[i]}em` }} className="inline-block font-semibold skeleton-box rounded-sm text-transparent">Loading...</span>
+          <span className="text-transparent rounded-sm"><LineSkeleton width={3} /></span>
+          <span className="inline-block font-semibold rounded-sm text-transparent"><LineSkeleton width={skeletonLengths[i]} /></span>
         </li>
       ))
   }</>
